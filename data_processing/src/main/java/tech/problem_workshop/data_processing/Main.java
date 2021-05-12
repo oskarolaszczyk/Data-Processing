8aa6670d2710fa872f80f52630311e1e51b4cfdc
oskarolaszczyk <oskarolaszczyk@gmail.com>
2021-05-13 01:22:38 +0200

    removing stories with null text

--- data_processing/src/main/java/tech/problem_workshop/data_processing/Main.java
+++ data_processing/src/main/java/tech/problem_workshop/data_processing/Main.java
@@ -8,11 +8,19 @@ import tech.problem_workshop.data_processing.model.Story;
 import tech.problem_workshop.data_processing.utils.PropertiesLoader;
 
 import java.io.IOException;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         StoryDAO storyDAO = new ResourcesStoryDao();
         var x = storyDAO.getAll("Data/" + PropertiesLoader.getJsonName());
+        x.removeIf(new Predicate<Story>() {
+            @Override
+            public boolean test(Story story) {
+                return story.getTextAfterStem().length == 0 ;
+            }
+        });
+
         // Potrzebne do debugowania i podejrzenia co jest wewnątrz
         var y = 1;
 
